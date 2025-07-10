e9c8d3af1b8a87af1af36e3bdd7cc1fd8e8b6f8a
aldentantan <aldentanfk@gmail.com>
2025-07-10 23:06:32 +0800

    Add files for custom form components

---
+++ src/components/form-components/CustomTextField.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { TextField } from '@mui/material'
+
+export const CustomTextField = ({ field, form, ...props }) => (
+  <TextField
+    {...field}
+    {...props}
+    fullWidth
+    margin='normal'
+    error={form.touched[field.name] && Boolean(form.errors[field.name])}
+    helperText={form.touched[field.name] && form.errors[field.name]}
+  />
+)
