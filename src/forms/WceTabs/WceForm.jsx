b2923b965710fd50a1564b95070a8f6c15686bb2
aldentantan <aldentanfk@gmail.com>
2025-07-19 14:28:57 +0800

    Refactor, update hxFamily questions and all references to it

--- src/forms/WceTabs/WceForm.jsx
+++ src/forms/WceTabs/WceForm.jsx
@@ -62,7 +62,7 @@ const initialValues = {
 // Custom Radio Field Component
 const RadioField = ({ name, label, options, ...props }) => {
   const { values, errors, touched, setFieldValue } = useFormikContext()
-  
+
   return (
     <FormControl error={touched[name] && !!errors[name]} {...props}>
       <FormLabel component="legend">{label}</FormLabel>
@@ -71,11 +71,11 @@ const RadioField = ({ name, label, options, ...props }) => {
         onChange={(e) => setFieldValue(name, e.target.value)}
       >
         {options.map((option) => (
-          <FormControlLabel 
+          <FormControlLabel
             key={option.value}
-            value={option.value} 
-            control={<Radio />} 
-            label={option.label} 
+            value={option.value}
+            control={<Radio />}
+            label={option.label}
           />
         ))}
       </RadioGroup>
@@ -232,78 +232,78 @@ const WceForm = (props) => {
                 <Form>
                   <div className='form--div fieldPadding'>
                     <h1>WCE</h1>
-                    
+
                     <h3>Completed Breast Self Examination station?</h3>
-                    <RadioField 
-                      name='wceQ3' 
-                      label='WCE Q3' 
-                      options={formOptions.wceQ3} 
+                    <RadioField
+                      name='wceQ3'
+                      label='WCE Q3'
+                      options={formOptions.wceQ3}
                     />
 
                     <h3>Completed Cervical Education station?</h3>
-                    <RadioField 
-                      name='wceQ4' 
-                      label='WCE Q4' 
-                      options={formOptions.wceQ4} 
+                    <RadioField
+                      name='wceQ4'
+                      label='WCE Q4'
+                      options={formOptions.wceQ4}
                     />
 
                     <h3>
                       When, if any, was the last hpv test you have taken? (Please verify on health hub) (HPV is different from Pap Smear, answer Pap Smear in the next question)
                     </h3>
-                    <RadioField 
-                      name='wceQ8' 
-                      label='WCE Q8' 
-                      options={formOptions.wceQ8} 
+                    <RadioField
+                      name='wceQ8'
+                      label='WCE Q8'
+                      options={formOptions.wceQ8}
                     />
 
                     <h3>
                       When if any, was the last Pap Smear test you have taken? (Please verify on health hub)
                     </h3>
-                    <RadioField 
-                      name='wceQ11' 
-                      label='WCE Q11' 
-                      options={formOptions.wceQ11} 
+                    <RadioField
+                      name='wceQ11'
+                      label='WCE Q11'
+                      options={formOptions.wceQ11}
                     />
 
                     <h3>
                       I am asking the next few questions to check your eligibility for a Pap Smear. This question may be sensitive, but could I ask if you have engaged in sexual intercourse before?
                     </h3>
-                    <RadioField 
-                      name='wceQ9' 
-                      label='WCE Q9' 
-                      options={formOptions.wceQ9} 
+                    <RadioField
+                      name='wceQ9'
+                      label='WCE Q9'
+                      options={formOptions.wceQ9}
                     />
 
                     <h3>Are you pregnant?</h3>
-                    <RadioField 
-                      name='wceQ10' 
-                      label='WCE Q10' 
-                      options={formOptions.wceQ10} 
+                    <RadioField
+                      name='wceQ10'
+                      label='WCE Q10'
+                      options={formOptions.wceQ10}
                     />
 
                     <h3>
                       Was your last menstrual period within the window where the first day falls between 29 July and 5 August 2024? If you are post-menopausal or use contraception, please indicate &apos;yes&apos;
                     </h3>
-                    <RadioField 
-                      name='wceQ12' 
-                      label='WCE Q12' 
-                      options={formOptions.wceQ12} 
+                    <RadioField
+                      name='wceQ12'
+                      label='WCE Q12'
+                      options={formOptions.wceQ12}
                     />
 
                     <h3>Indicated interest for HPV Test under SCS?</h3>
-                    <RadioField 
-                      name='wceQ5' 
-                      label='WCE Q5' 
-                      options={formOptions.wceQ5} 
+                    <RadioField
+                      name='wceQ5'
+                      label='WCE Q5'
+                      options={formOptions.wceQ5}
                     />
 
                     <h3>
                       Is patient indicated for on-site testing? Please circle On-Site Testing on Form A as well
                     </h3>
-                    <RadioField 
-                      name='wceQ7' 
-                      label='WCE Q7' 
-                      options={formOptions.wceQ7} 
+                    <RadioField
+                      name='wceQ7'
+                      label='WCE Q7'
+                      options={formOptions.wceQ7}
                     />
 
                     <h3>HPV Test Eligibility</h3>
@@ -314,9 +314,9 @@ const WceForm = (props) => {
                     {loading ? (
                       <CircularProgress />
                     ) : (
-                      <Button 
-                        type='submit' 
-                        variant='contained' 
+                      <Button
+                        type='submit'
+                        variant='contained'
                         color='primary'
                         disabled={loading}
                       >
@@ -366,21 +366,6 @@ const WceForm = (props) => {
               )}
               {hxSocial && hxSocial.SOCIALShortAns3 ? (
                 <p className='blue'>{hxSocial.SOCIALShortAns3}</p>
-              ) : (
-                <p className='blue'>nil</p>
-              )}
-
-              <h2>Family History</h2>
-              <p className='underlined'>Is there positive family history{' '}
-                <span className='red'>(AMONG FIRST DEGREE RELATIVES)</span> for the following cancers?:</p>
-              {hxFamily && hxFamily.FAMILY1 ? (
-                <p className='blue'>{hxFamily.FAMILY1}</p>
-              ) : (
-                <p className='blue'>nil</p>
-              )}
-              <p className='underlined'>Age of diagnosis:</p>
-              {hxFamily && hxFamily.FAMILYShortAns1 ? (
-                <p className='blue'>{hxFamily.FAMILYShortAns1}</p>
               ) : (
                 <p className='blue'>nil</p>
               )}
