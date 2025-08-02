456960814b3e7bec65e7d785a7fc18542d916e9a
xin-yi-phang <158749925+xin-yi-phang@users.noreply.github.com>
2025-08-02 23:12:51 +0800

    Update VaccineForm.jsx

--- src/forms/VaccineForm.jsx
+++ src/forms/VaccineForm.jsx
@@ -1,36 +1,25 @@
 import React, { useContext, useEffect, useState } from 'react'
 import {
-  Paper, Divider, Typography, CircularProgress,
-  FormControl, FormLabel, RadioGroup, FormControlLabel,
-  Radio, Button, Grid
+  Paper, 
+  Divider, 
+  Typography, 
+  CircularProgress,
+  Button, 
+  Grid,
+  Box
 } from '@mui/material'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { Formik, Form, Field } from 'formik'
 import * as Yup from 'yup'
 import { FormContext } from '../api/utils.js'
 import { getSavedData } from '../services/mongoDB'
 import { submitForm } from '../api/api.jsx'
 import allForms from './forms.json'
+import CustomRadioGroup from '../components/form-components/CustomRadioGroup'
 import ErrorNotification from '../components/form-components/ErrorNotification'
 import './fieldPadding.css'
 import { useNavigate } from 'react-router'
 
 const formName = 'vaccineForm'
-
-const RadioGroupField = ({ name, label, values }) => (
-  <FormControl fullWidth sx={{ mb: 3 }}>
-    <FormLabel><Typography variant="subtitle1" fontWeight="bold">{label}</Typography></FormLabel>
-    <Field name={name}>
-      {({ field }) => (
-        <RadioGroup {...field} row>
-          {values.map((val) => (
-            <FormControlLabel key={val} value={val} control={<Radio />} label={val} />
-          ))}
-        </RadioGroup>
-      )}
-    </Field>
-    <ErrorMessage name={name} component="div" style={{ color: 'red' }} />
-  </FormControl>
-)
 
 const initialValues = {
   VAX1: ''
@@ -47,8 +36,16 @@ export default function VaccineForm() {
   const validationSchema = Yup.object({
     VAX1: Yup.string()
       .oneOf(['Yes', 'No'], 'Please select Yes or No')
-      .required('Required')
+      .required('This field is required')
   })
+
+  // Form options following the pattern from other forms
+  const formOptions = {
+    VAX1: [
+      { label: 'Yes', value: 'Yes' },
+      { label: 'No', value: 'No' }
+    ]
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -66,9 +63,13 @@ export default function VaccineForm() {
 
   const handleSubmit = async (values, { setSubmitting }) => {
     setLoading(true)
+    setSubmitting(true)
+    
     const response = await submitForm(values, patientId, formName)
+    
     setLoading(false)
     setSubmitting(false)
+    
     if (response.result) {
       setTimeout(() => {
         alert('Successfully submitted form')
@@ -94,24 +95,48 @@ export default function VaccineForm() {
             >
               {({ isSubmitting, errors, submitCount }) => (
                 <Form className="fieldPadding">
-                  <Typography variant="h6" gutterBottom>Vaccination</Typography>
-                  <Typography variant="subtitle1" gutterBottom>
-                    You have signed up for your complimentary influenza vaccination.
-                  </Typography>
+                  <div className='form--div'>
+                    <Typography variant="h4" component="h1" gutterBottom>
+                      Vaccination
+                    </Typography>
+                    
+                    <Typography variant="subtitle1" gutterBottom>
+                      You have signed up for your complimentary influenza vaccination.
+                    </Typography>
 
-                  <RadioGroupField name="VAX1" label="VAX1" values={["Yes", "No"]} />
+                    <Typography variant="h6" component="h3" gutterBottom>
+                      Do you consent to receiving the influenza vaccination?
+                    </Typography>
+                    
+                    <Field
+                      name="VAX1"
+                      component={CustomRadioGroup}
+                      options={formOptions.VAX1}
+                      label="VAX1"
+                      row
+                    />
+                  </div>
 
                   <ErrorNotification 
                     show={submitCount > 0 && Object.keys(errors).length > 0}
                   />
 
-                  <div style={{ marginTop: 16, display: 'flex', justifyContent: 'center' }}>
-                    {loading || isSubmitting ? <CircularProgress /> : (
-                      <Button type="submit" variant="contained" color="primary">
+                  <Box sx={{ mt: 2, display: 'flex', justifyContent: 'center' }}>
+                    {loading || isSubmitting ? (
+                      <CircularProgress />
+                    ) : (
+                      <Button 
+                        type="submit" 
+                        variant="contained" 
+                        color="primary"
+                        size="large"
+                        disabled={isSubmitting}
+                      >
                         Submit
                       </Button>
                     )}
-                  </div>
+                  </Box>
+                  
                   <br />
                   <Divider />
                 </Form>
@@ -119,6 +144,7 @@ export default function VaccineForm() {
             </Formik>
           </Paper>
         </Grid>
+        
         <Grid
           p={1}
           width='30%'
@@ -133,11 +159,17 @@ export default function VaccineForm() {
               <Typography variant="h6" gutterBottom>Patient Info</Typography>
               {regi ? (
                 <>
-                  <Typography variant="body1" className='blue'>Age: {regi.registrationQ4}</Typography>
-                  <Typography variant="body1" className='blue'>Citizenship: {regi.registrationQ7}</Typography>
+                  <Typography variant="body1" className='blue'>
+                    Age: {regi.registrationQ4}
+                  </Typography>
+                  <Typography variant="body1" className='blue'>
+                    Citizenship: {regi.registrationQ7}
+                  </Typography>
                 </>
               ) : (
-                <Typography variant="body1" className='red'>NO REGI DATA</Typography>
+                <Typography variant="body1" className='red'>
+                  NO REGI DATA
+                </Typography>
               )}
             </div>
           )}
